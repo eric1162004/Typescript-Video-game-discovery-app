64eb5e80207f2e1852276e0e72fea46a72a369d2
Eric Cheung <eric1162004@hotmail.com>
2024-05-26 18:57:10 -0700

    fix axios config

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "f47882f369104a06910351023395438f",
+    key: import.meta.env.VITE_GAME_API,
   },
 });
