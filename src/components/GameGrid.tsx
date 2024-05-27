110c08ebb158ce87eac7b1ab5dc1caf6cd01351f
Eric Cheung <eric1162004@hotmail.com>
2024-05-26 17:22:27 -0700

    sort the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,10 +7,10 @@ import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
 interface Props {
-  gameQuery: GameQuery,
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({gameQuery }: Props) => {
+const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
